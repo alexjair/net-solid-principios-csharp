d11c9f599d79ecd31ff5aeccdbfd80bcbad458a4
Alexander Jair <alexjair@gmail.com>
2023-06-03 14:41:20 -0500

    agreago los originales

---
+++ 2-OpenClose - Corregido (Interfaces)/Program.cs
@@ -0,0 +1,15 @@
+﻿using OpenClose;
+
+CalculateSalaryMonthly(new List<IEmployee>() {
+    new EmployeeFullTime("Pepito Pérez", 160),
+    new EmployeePartTime("Manuel Lopera", 180)
+});
+
+
+void CalculateSalaryMonthly(List<IEmployee> employees)
+{
+    foreach (var employee in employees)
+    {
+        Console.WriteLine($"Empleado: {employee.Fullname}, Pago: {employee.CalculateSalaryMonthly()} ");
+    }
+}
